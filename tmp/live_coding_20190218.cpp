a8998fdd286fc37b5a8c2a736b4be9c6d63370b7
Christos Gogos <chgogos@gmail.com>
2019-02-21 00:21:06 +0200

    live coding 18/2/2019

---
+++ tmp/live_coding_20190218.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Rectangle
+{
+  public:
+    double width, height;
+    double area()
+    {
+        return width * height;
+    }
+};
+
+int main()
+{
+    // Rectangle r1;
+    // r1.width=5.2;
+    // r1.height=4.6;
+    // std::cout << r1.area() << std::endl;
+
+    Rectangle *r = new Rectangle();
+    r->width = 7.1;
+    r->height = 2.3;
+    delete r;
+    std::cout << r->area() << std::endl;
+}
