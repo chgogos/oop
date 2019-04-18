4f2f42b60464e45cb9c838df307694fde3a62fed
Christos Gogos <chgogos@gmail.com>
2019-04-18 12:26:51 +0300

    fix

--- cpp_playground/ex071/lambda1.cpp
+++ cpp_playground/ex071/lambda1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <functional>
 
 int main()
 {
 
+    // std::function<int(int, int)> lambda1 = [](double x, double y) { return x * y; };
     auto lambda1 = [](double x, double y) { return x * y; };
     std::cout << lambda1(5, 6) << std::endl;
 
@@ -16,5 +18,7 @@ int main()
 
     auto lambda4 = [&](double x, double y) { a*=10; b*=10; return a * b * x * y; };
     std::cout << lambda4(5, 6) << " value of a=" << a << std::endl;
+
+    
     return 0;
 }
