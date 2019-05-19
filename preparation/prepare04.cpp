71aa4e579336e67ff6616209d11671e5ea5c1022
Christos Gogos <chgogos@gmail.com>
2019-05-19 23:11:09 +0300

    prepare

---
+++ preparation/prepare04.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class person
+{
+private:
+    std::string name;
+
+public:
+    person(){};
+    person(std::string n) : name(n) {}
+    std::string get_name()
+    {
+        return name;
+    }
+};
+
+class movie
+{
+private:
+    std::string title;
+    int year;
+    person director;
+    std::vector<person> actors;
+
+public:
+    movie(std::string t) : title(t) {}
+
+    void set_director(person &p)
+    {
+        director = p;
+    }
+
+    void add_cast(person &p)
+    {
+        actors.push_back(p);
+    }
+
+    void display_info()
+    {
+        std::cout << title << " ";
+        std::cout << "directed by " << director.get_name() << std::endl;
+        for (auto p : actors)
+        {
+            std::cout << p.get_name() << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    person d1("director 1");
+    person d2("director 2");
+    person a1("actor1");
+    person a2("actor2");
+    person a3("actor3");
+    movie m1("Movie 1");
+    movie m2("Movie 2");
+
+    m1.set_director(d1);
+    m2.set_director(d2);
+    m1.add_cast(a1);
+    m1.add_cast(a2);
+    m2.add_cast(a2);
+    m2.add_cast(a3);
+
+    m1.display_info();
+    m2.display_info();
+
+    return 0;
+}
