1f9b902fa1b5281b7c50081ac8985aebf98b40b2
Christos Gogos <chgogos@gmail.com>
2020-04-30 12:31:58 +0300

    dbg example visual studio

--- various/DEBUG/README.md
+++ various/DEBUG/README.md
@@ -1,1 +1,10 @@
 # Debug examples
+
+<!-- ## GDB -->
+
+## VS Code
+
+* <https://dev.to/jerrygoyal/run-debug-get-intellisense-for-c-c-in-vscode-4e0o>
+* <https://code.visualstudio.com/docs/cpp/launch-json-reference>
+
+## Visual Studio
--- various/DEBUG/dbg1.cpp
+++ various/DEBUG/dbg1.cpp
@@ -5,18 +5,17 @@ using std::endl;
 
 int main()
 {
-    // int b[10] = {0};
-    // for (int i = 0; i < 10; i++)
-    //     cout << "b[" << i << "]=" << &b[i] << endl;
     int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for (int i = 0; i < 10; i++)
-        cout << "a[" << i << "]=" << &a[i] << endl;
     int sum = a[0];
-    cout << &sum << endl;
     for (int i = 1; i <= 10; i++)
     {
-        cout << i << " " << a[i]  << " " << &a[i] << endl;
         sum += a[i];
     }
     cout << sum << endl;
 }
+
+/*
+65
+ΛΑΘΟΣ ΑΠΟΤΕΛΕΣΜΑ ΕΠΡΕΠΕ ΝΑ ΕΙΝΑΙ 55
+*/
+
