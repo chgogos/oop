847f4737d8dbe0cf8f4567d376eb15f4e47d2da7
Christos Gogos <chgogos@gmail.com>
2020-05-13 10:24:50 +0300

    tmp3

---
+++ lab2020/tmp/tmp3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// επιστροφή του πλήθους των εμφανίσων της μικρότερης τιμής
+int occurences(vector<int> v)
+{
+
+    return 0;
+}
+
+int main()
+{
+    vector<int> v1{6, 9, 2, 3, 2, 9, 2};
+    int c = occurences(v1);
+    cout << "Result " << c << endl;
+}
