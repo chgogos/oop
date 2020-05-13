9e6fb182650729cb8ed919019cd2c9ffc6596baa
Christos Gogos <chgogos@gmail.com>
2020-05-13 11:06:55 +0300

    step 4

--- lab2020/tmp/tmp3.cpp
+++ lab2020/tmp/tmp3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ void occurences(vector<T> v, int &c, T &min)
         if (v[i] < min)
             min = v[i];
     }
-    c=0;
+    c = 0;
     for (int i = 0; i < v.size(); i++)
     {
         if (v[i] == min)
@@ -21,35 +22,50 @@ void occurences(vector<T> v, int &c, T &min)
     }
 }
 
-class player{
-    public:
-        string name;
-        int time_played;
+class player
+{
+public:
+    string name;
+    int time_played;
 
-        bool operator<(const player &other){
-            return time_played < other.time_played;
-        }
-        bool operator==(const player &other){
-            return time_played == other.time_played;
-        }
+    bool operator<(const player &other)
+    {
+        return time_played < other.time_played;
+    }
+    bool operator==(const player &other)
+    {
+        return time_played == other.time_played;
+    }
+    operator string() const{
+        stringstream ss;
+        ss << name << " - " << time_played;
+        return ss.str();
+    }
 };
 
 int main()
 {
     vector<int> v1{2, 6, 9, 2, 3, 2, 9, 2};
-    int c1, min1; 
+    int c1, min1;
     occurences(v1, c1, min1);
-    cout << "Min: "<< min1 <<" occurs: " << c1 << endl;
+    cout << "Min: " << min1 << " occurs: " << c1 << endl;
 
     vector<double> v2{2.1, 6.3, 1.1, 2.1, 1.1, 2.0, 9.1, 2.5};
     int c2;
-    double min2; 
+    double min2;
     occurences(v2, c2, min2);
-    cout << "Min: "<< min2 <<" occurs: " << c2 << endl;
+    cout << "Min: " << min2 << " occurs: " << c2 << endl;
 
-    vector<player> v3{{"nikos", 30},{"maria", 5},{"kostas", 60}, {"niki",5}};
+    vector<player> v3{{"nikos", 30}, {"maria", 5}, {"kostas", 60}, {"niki", 5}};
     int c3;
     player p;
     occurences(v3, c3, p);
-    cout << "Min: "<< p.time_played <<" occurs: " << c3 << endl;
+    cout << "Min: " << p.time_played << " occurs: " << c3 << endl;
+
+    for (player a_player : v3)
+    {
+        cout << a_player.name << " " << a_player.time_played << endl;
+        string ps = a_player;
+        cout << ps << endl;
+    }
 }
