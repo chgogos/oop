02d08ff9aa0d8de232d374a04eb298ca48c1d460
Christos Gogos <chgogos@gmail.com>
2020-05-20 11:59:44 +0300

    iterator

--- lab2020/tmp/tmp9.cpp
+++ lab2020/tmp/tmp9.cpp
@@ -10,7 +10,12 @@ int main()
 {
     vector<int> v{7, 8, 1, 2, 44, 5};
 
-    
+    auto itr=v.begin();
+    while(itr != v.end()){
+        if ((*itr) % 2 == 0)
+            *itr = (*itr) * 2;
+        itr++;
+    }
 
     for(auto x: v){
         cout << x << " ";
