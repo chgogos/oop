9b5dfbc89869c2a3c28db69fd52a995f8808e669
Christos Gogos <chgogos@gmail.com>
2020-05-20 11:27:52 +0300

    map

--- lab2020/tmp/tmp6.cpp
+++ lab2020/tmp/tmp6.cpp
@@ -3,17 +3,18 @@
 // εμφάνιση του vector
 
 #include <vector>
+#include <list>
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    vector<double> v{2.1, 3.2, 5.3, 1.1, 7.3};
+    list<double> v{2.1, 3.2, 5.3, 1.1, 7.3};
     double sum =0.0;
     for(auto x: v){
         sum += x;
     }
-    v.push_back(sum/v.size());
+    v.push_front(sum/v.size());
     for(auto x: v){
         cout << x << " ";
     }
