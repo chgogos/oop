7def589276947cdaf7721e36371d053dafd260b4
Christos Gogos <chgogos@gmail.com>
2020-05-20 11:48:39 +0300

    sort

--- lab2020/tmp/tmp8.cpp
+++ lab2020/tmp/tmp8.cpp
@@ -3,6 +3,8 @@
 // εμφάνιση
 // ανακάτεμα των 4 πρώτων {8,6,3,1,9,13} 
 // εμφάνιση
+// ταξινόμηση σε φθίνουσα σειρά {13,9,8,6,3,1}
+// εμφάνιση
 
 #include <vector>
 #include <iostream>
@@ -13,9 +15,21 @@ using namespace std;
 int main(){
     vector<int> v{3,13,6,1,8,9};
 
+    sort(v.begin(), v.end());
     for(auto x: v){
         cout << x << " ";
     }
     cout << endl;
 
+    random_shuffle(v.begin(), v.begin()+4);
+    for(auto x: v){
+        cout << x << " ";
+    }
+    cout << endl;
+
+    sort(v.begin(), v.end(), greater<int>());
+        for(auto x: v){
+        cout << x << " ";
+    }
+    cout << endl;
 }
