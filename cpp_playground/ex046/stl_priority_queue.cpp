89e2db75e6807b0ea425fc9a02126a1f7754cf97
Christos Gogos <chgogos@gmail.com>
2020-05-24 09:56:52 +0300

    priority_queue

--- cpp_playground/ex046/stl_priority_queue.cpp
+++ cpp_playground/ex046/stl_priority_queue.cpp
@@ -7,33 +7,64 @@ using namespace std;
 
 int main()
 {
-    priority_queue<int> a_priority_queue;
+    // priority_queue<int> a_priority_queue;  // MAX HEAP
+    priority_queue<int,vector<int>,less<int>> a_priority_queue;
     for (int x : {7,2,5,8,9,1})
     {
         a_priority_queue.push(x);
-        cout << "PRIORITY QUEUE SIZE:" << a_priority_queue.size() << " TOP:" << a_priority_queue.top() << endl;
+        cout << "PRIORITY QUEUE(MAX HEAP) SIZE:" << a_priority_queue.size() << " TOP:" << a_priority_queue.top() << endl;
     }
 
     cout << "#############################" << endl;
     while (!a_priority_queue.empty())
     {
-        cout << "PRIORITY QUEUE SIZE:" << a_priority_queue.size() << " TOP:" << a_priority_queue.top() << endl;
+        cout << "PRIORITY QUEUE(MAX HEAP) SIZE:" << a_priority_queue.size() << " TOP:" << a_priority_queue.top() << endl;
         a_priority_queue.pop();
+    }
+
+    cout << "#############################" << endl;
+
+    priority_queue<int,vector<int>,greater<int>> a_priority_queue2; // MIN HEAP
+    for (int x : {7,2,5,8,9,1})
+    {
+        a_priority_queue2.push(x);
+        cout << "PRIORITY QUEUE(MIN HEAP) SIZE:" << a_priority_queue2.size() << " TOP:" << a_priority_queue2.top() << endl;
+    }
+
+    cout << "#############################" << endl;
+    while (!a_priority_queue2.empty())
+    {
+        cout << "PRIORITY QUEUE(MIN HEAP) SIZE:" << a_priority_queue2.size() << " TOP:" << a_priority_queue2.top() << endl;
+        a_priority_queue2.pop();
     }
 }
 
 /*
-PRIORITY QUEUE SIZE:1 TOP:7
-PRIORITY QUEUE SIZE:2 TOP:7
-PRIORITY QUEUE SIZE:3 TOP:7
-PRIORITY QUEUE SIZE:4 TOP:8
-PRIORITY QUEUE SIZE:5 TOP:9
-PRIORITY QUEUE SIZE:6 TOP:9
+PRIORITY QUEUE(MAX HEAP) SIZE:1 TOP:7
+PRIORITY QUEUE(MAX HEAP) SIZE:2 TOP:7
+PRIORITY QUEUE(MAX HEAP) SIZE:3 TOP:7
+PRIORITY QUEUE(MAX HEAP) SIZE:4 TOP:8
+PRIORITY QUEUE(MAX HEAP) SIZE:5 TOP:9
+PRIORITY QUEUE(MAX HEAP) SIZE:6 TOP:9
 #############################
-PRIORITY QUEUE SIZE:6 TOP:9
-PRIORITY QUEUE SIZE:5 TOP:8
-PRIORITY QUEUE SIZE:4 TOP:7
-PRIORITY QUEUE SIZE:3 TOP:5
-PRIORITY QUEUE SIZE:2 TOP:2
-PRIORITY QUEUE SIZE:1 TOP:1
+PRIORITY QUEUE(MAX HEAP) SIZE:6 TOP:9
+PRIORITY QUEUE(MAX HEAP) SIZE:5 TOP:8
+PRIORITY QUEUE(MAX HEAP) SIZE:4 TOP:7
+PRIORITY QUEUE(MAX HEAP) SIZE:3 TOP:5
+PRIORITY QUEUE(MAX HEAP) SIZE:2 TOP:2
+PRIORITY QUEUE(MAX HEAP) SIZE:1 TOP:1
+#############################
+PRIORITY QUEUE(MIN HEAP) SIZE:1 TOP:7
+PRIORITY QUEUE(MIN HEAP) SIZE:2 TOP:2
+PRIORITY QUEUE(MIN HEAP) SIZE:3 TOP:2
+PRIORITY QUEUE(MIN HEAP) SIZE:4 TOP:2
+PRIORITY QUEUE(MIN HEAP) SIZE:5 TOP:2
+PRIORITY QUEUE(MIN HEAP) SIZE:6 TOP:1
+#############################
+PRIORITY QUEUE(MIN HEAP) SIZE:6 TOP:1
+PRIORITY QUEUE(MIN HEAP) SIZE:5 TOP:2
+PRIORITY QUEUE(MIN HEAP) SIZE:4 TOP:5
+PRIORITY QUEUE(MIN HEAP) SIZE:3 TOP:7
+PRIORITY QUEUE(MIN HEAP) SIZE:2 TOP:8
+PRIORITY QUEUE(MIN HEAP) SIZE:1 TOP:9
 */
