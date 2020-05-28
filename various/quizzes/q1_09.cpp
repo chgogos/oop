59c9bb536f1dd5ddaf4f95e8f62a91fe05808cdb
Christos Gogos <chgogos@gmail.com>
2020-05-28 22:35:02 +0300

    quiz

--- various/quizzes/q1_09.cpp
+++ various/quizzes/q1_09.cpp
@@ -1,5 +1,5 @@
 /*
-Quiz 1 - 27/5/2020
+Quiz 1
 */
 
 #include <iostream>
@@ -9,6 +9,7 @@ class Box
 {
 private:
     double width, depth, height;
+
 public:
     Box(double w, double d, double h) : width(w), depth(d), height(h) {}
     double get_volume() { return width * height * depth; }
@@ -17,12 +18,12 @@ int main()
 {
     Box b1(1.0, 2.0, 2.0);
     Box b2(3.0, 2.0, 2.0);
-    if (b1.get_volume() < b2.get_volume())
+    if (b1.get_volume() > b2.get_volume())
         cout << "b1";
     else
         cout << "b2";
 }
 
 /*
-b1
+b2
 */
