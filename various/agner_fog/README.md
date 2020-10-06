8a8d60178b92767619c3213d09adce9f5fd161c0
Christos Gogos <chgogos@gmail.com>
2020-10-06 19:44:48 +0300

    fog

---
+++ various/agner_fog/README.md
@@ -0,0 +1,6 @@
+# Software optimization resources by Agner Fog
+
+https://www.agner.org/optimize/
+
+
+* [matrix](./matrix.cpp) δυναμικός δισδιάστατος πίνακας
---
+++ various/agner_fog/matrix.cpp
@@ -0,0 +1,67 @@
+// Optimizing software in C++
+
+#include <iostream>
+#include <cstdio>
+
+
+template <typename T>
+class Matrix
+{
+public:
+    Matrix(int rows, int columns)
+    {
+        this->rows = rows;
+        this->columns = columns;
+        p = new T[rows * columns];
+        if (p == 0)
+        {
+            std::cerr << "Allocation failed" << std::endl;
+        }
+        else
+        {
+            for (int i = 0; i < rows * columns; i++)
+            {
+                p[i] = 0;
+            }
+        }
+    }
+    int nrows() const
+    {
+        return rows;
+    }
+
+    int ncols() const
+    {
+        return columns;
+    }
+
+    ~Matrix()
+    {
+        if (p)
+            delete[] p;
+    }
+
+    T *operator[](int r)
+    {
+        return p + r * columns;
+    }
+
+protected:
+    int rows;
+    int columns;
+    T *p;
+};
+
+int main()
+{
+    Matrix<double> mat(8, 10);
+    mat[3][5] = 12.34;
+    for (int r = 0; r < mat.nrows(); r++)
+    {
+        for (int c = 0; c < mat.ncols(); c++)
+        {
+            printf("%5.2f ", mat[r][c]);
+        }
+        printf("\n");
+    }
+}
