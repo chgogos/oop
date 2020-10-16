f019d94328f7dde72565f2fbd83a2b146d9b4650
Christos Gogos <chgogos@gmail.com>
2020-10-16 03:30:15 +0300

    getline

---
+++ cpp_playground/ex094/cin_getline.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "enter input with spaces: ";
+    getline(cin, s);
+    cout << s;
+}
+
+/*
+enter input with spaces: text with spaces as input
+text with spaces as input
+*/
