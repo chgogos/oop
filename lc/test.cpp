a349dc45b9cf4742bd81d74779a42118df076cb3
Christos Gogos <chgogos@gmail.com>
2020-11-06 11:30:08 +0200

    test

--- lc/test.cpp
+++ lc/test.cpp
@@ -2,16 +2,12 @@
 
 using namespace std;
 
-void fun(int a, int b, int sum, int prod){
-    sum = a+b;
-    prod = a*b;
+int fun(const int a){
+    a *= 2;
+    return a;
 }
 
 int main(){
-    int x, y, z=0, w=0;
-    x = 1;
-    y = 2;
-    fun(x,y,z,w);
-    cout << "SUM " << z << endl;
-    cout << "PROD " << w << endl;
+    int x = 5;
+    cout << fun(x) << endl;
 }
