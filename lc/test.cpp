f25cab6d8e2cbcb67c2d60a7f38601915a1d1d8c
Christos Gogos <chgogos@gmail.com>
2020-11-06 12:00:36 +0200

    test

--- lc/test.cpp
+++ lc/test.cpp
@@ -9,14 +9,23 @@ class Cargo{
         double weight;
         bool fragile;
     public:
-
+        Cargo(): id(0), destination("UNDEFINED"), weight(.0), fragile(false) {}
+        Cargo(int i, string d, double w, bool f): id(i), destination(d), weight(w), fragile(f) {}
         void show(){
             cout << "ID:" << id << " DESTINATION:" << destination << " WEIGHT:" << weight << " FRAGILE:" << fragile << endl; 
         }
+
+        double get_weight(){
+            return weight;
+        }
+
+        void set_weight(double w){
+            weight = w;
+        }
 };
 
-
 int main(){
-        Cargo c1;
+        Cargo c1, c2(12345, "ATHENS", 1000, true);
         c1.show();
+        c2.show();
 }
