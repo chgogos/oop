02dae2e2c4dc2df36f181f6f09592447ad277c2f
Christos Gogos <chgogos@Christoss-MBP.lan>
2020-11-19 10:16:13 +0200

    test

--- lc/test1.cpp
+++ lc/test1.cpp
@@ -10,16 +10,18 @@ private:
 
 public:
     date() : day(1), month(1), year(2020){};
-    date(int d, int m) : day(d), month(m), year(2020){};     
+    date(int d, int m) : day(d), month(m), year(2020){};
     date(int d, int m, int y) : day(d), month(m), year(y){};
     int get_day() const { return day; }
     int get_month() const { return month; }
     int get_year() const { return year; }
-    void add_one_day(){
+    void add_one_day()
+    {
         // λειτουργεί μόνο για την τρέχουσα ημερομηνία (19/11/2020)
         day++;
-        if (day == 32){
-            day=1;
+        if (day == 32)
+        {
+            day = 1;
             month++;
         }
     }
@@ -32,22 +34,24 @@ private:
     string name;
 
 public:
-    holiday(string n) : date(), name(n) {} // constructor που καλεί τον constructor της βασικής κλάσης
+    holiday(string n) : date(), name(n) {}                   // constructor που καλεί τον constructor της βασικής κλάσης
     holiday(string n, int d, int m) : date(d, m), name(n) {} // constructor που καλεί τον constructor της βασικής κλάσης
     string get_name() const { return name; }
     virtual string desc() const
     {
         return name + " " + date::desc();
     }
+    void add_one_day(){
+        cout << "This is a holiday ..." << endl;
+    }
 };
 
 int main()
 {
-    holiday h("Christmas", 25, 12);
-    cout << h.desc() << endl; // Christmas 25/12/2020
+    date *obj = new holiday("Christmas", 25,12);
+    cout << obj->desc() << endl;
+    obj->add_one_day();
+    cout << obj->desc() << endl;
 
-    date d(19,11,2020);
-    d.add_one_day();
-    cout << d.desc() << endl;
-
+    delete obj;
 }
