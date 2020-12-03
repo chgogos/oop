e6b6b2dfaefee42390f0ee769b6ad1c7b22d3d6e
Christos Gogos <chgogos@gmail.com>
2020-12-03 09:41:34 +0200

    test

--- lc/test1.cpp
+++ lc/test1.cpp
@@ -6,9 +6,10 @@
 
 using namespace std;
 
-int my_fun(int a[5], int key){
+template <typename T>
+int my_fun(T* a, int size, T key){
     int c = 0;
-    for(int i=0;i<5;i++){
+    for(int i=0;i<size;i++){
         if (a[i]==key){
             c++;
         }
@@ -18,12 +19,12 @@ int my_fun(int a[5], int key){
 
 int main()
 {
-    int a[5]={2,3,2,2,4};
-    int res = my_fun(a, 2);
+    int a[]={2,3,2,2,4,5,7,8,9,2};
+    int res = my_fun(a, 10, 2);
     cout << res << endl;
 
-    // double b[5]={2.1,3.2,2.1,2.4,4.5};
-    // int res = my_fun(b, 2.1);
-    // cout << res << endl;
+    double b[5]={2.1,3.2,2.1,2.4,4.5};
+    res = my_fun(b, 5, 2.1);
+    cout << res << endl;
 }
 
