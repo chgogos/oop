495801fe4ac7d60438471171cdc688d1d3c14d80
Christos Gogos <chgogos@gmail.com>
2020-12-03 09:59:42 +0200

    test

--- lc/test1.cpp
+++ lc/test1.cpp
@@ -1,34 +1,23 @@
-// templated συνάρτηση που να δέχεται έναν πίνακα 5 θέσεων με στοιχεία τύπου Τ
-// και ένα στοιχείο προς αναζήτηση και να επιστρέφει πόσες φορές υπάρχει το
-// στοιχείο στον πίνακα
-
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-template <typename T>
-int my_fun(vector<T> a, T key)
-{
-    int c = 0;
-    for (int i = 0; i < a.size(); i++)
-    {
-        if (a[i] == key)
-        {
-            c++;
-        }
-    }
-    return c;
+void my_swap(int a, int b){
+    // TODO
 }
+
+void my_swap(string a, string b){
+    // TODO
+}
+
 
 int main()
 {
-    vector<int> a = {2, 3, 2, 2, 4, 5, 7, 8, 9, 2};
-    a.push_back(7);
-    int res = my_fun(a, 2);
-    cout << res << endl;
+    int x1=5, x2=10;
+    my_swap(x1,x2);
+    cout << "x1="<< x1 << " x2=" << x2 << endl;
 
-    vector<double> b = {2.1, 3.2, 2.1, 2.4, 4.5};
-    res = my_fun(b, 2.1);
-    cout << res << endl;
+    string s1="hello", s2="hi";
+    my_swap(s1,s2);
+    cout << "s1="<< s1 << " s2=" << s2 << endl;
 }
