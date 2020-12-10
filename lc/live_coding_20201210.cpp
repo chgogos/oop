b29e35a43d069b96d8b6bd4aa2baac90bee6458a
Christos Gogos <chgogos@gmail.com>
2020-12-10 10:02:15 +0200

    erg

--- lc/live_coding_20201210.cpp
+++ lc/live_coding_20201210.cpp
@@ -10,15 +10,21 @@ int main()
     for_each(v.begin(), v.end(), lambda);
 
     // διπλασιασμός κάθε στοιχείου του vector με for_each (1 γραμμή)
-    for_each(v.begin(), v.end(), [](int &x){x=3*x;});
+    for_each(v.begin(), v.end(), [](int &x) {x=3*x;});
 
     cout << "####################" << endl;
     for (int x : v) // range based for
     {
         cout << x << endl;
     }
-
+    
+    cout << "####################" << endl;
     // count_if (καταμέτρηση των περιττών αριθμών του vector)
     int c = count_if(v.begin(), v.end(), [](int x){return x%2==1;});
+    cout << c << endl;
+    
+    cout << "####################" << endl;
+    // count_if (καταμέτρηση των τιμών που είναι > 5)
+    c = count_if(v.begin(), v.end(), [](int x){return x>5;});
     cout << c << endl;
 }
