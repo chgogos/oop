cb75fd7ea88de3c6f099b8f26ca0dc4a451f8d9d
Christos Gogos <chgogos@gmail.com>
2020-12-10 10:33:56 +0200

    stl

---
+++ lc/live_coding_20201210c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<string, string> telephones;
+    telephones.insert({"nikos", "697123226"});
+    telephones.insert({"petros", "697122456"});
+    telephones.insert({"maria", "697223456"});
+
+    cout << telephones["maria"] << endl;
+
+    cout << "#######################" << endl;
+    for(auto kv: telephones){
+        cout << kv.first << " " << kv.second << endl;
+    }
+
+    // εμφάνιση όλων των στοιχείων του map με iterator
+    cout << "#######################" << endl;
+    
+}
