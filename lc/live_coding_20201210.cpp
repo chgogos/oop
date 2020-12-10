308692b34760c36052227d25f2325a1786bdb784
Christos Gogos <chgogos@gmail.com>
2020-12-10 09:24:02 +0200

    erg

--- lc/live_coding_20201210.cpp
+++ lc/live_coding_20201210.cpp
@@ -8,4 +8,13 @@ int main()
     auto lambda = [](int x) { cout << x * x << endl; };
     vector<int> v{1, 2, 3, 4, 5};
     for_each(v.begin(), v.end(), lambda);
+
+    // διπλασιασμός κάθε στοιχείου του vector με for_each (1 γραμμή)
+
+
+    cout << "####################" << endl;
+    for (int x : v) // range based for
+    {
+        cout << x << endl;
+    }
 }
