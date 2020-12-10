c956c524cdcff4a58819897bf0465c50b1b5ee02
Christos Gogos <chgogos@gmail.com>
2020-12-10 10:55:24 +0200

    stl

--- lc/live_coding_20201210d.cpp
+++ lc/live_coding_20201210d.cpp
@@ -15,16 +15,16 @@ int main()
     cout << endl;
 
     // 1. ταξινόμηση λίστας αλφαβητική
-
+    sort(v.begin(), v.end());
     cout << "####################" << endl;
     for (auto x : v)
     {
         cout << x << " ";
     }
     cout << endl;
-    
-    // 2. ταξινόμηση λίστας αλφαβητική σε φθίνουσα σειρά
 
+    // 2. ταξινόμηση λίστας αλφαβητική σε φθίνουσα σειρά
+    sort(v.begin(), v.end(), greater<string>());
     cout << "####################" << endl;
     for (auto x : v)
     {
@@ -33,11 +33,11 @@ int main()
     cout << endl;
 
     // 3. ταξινόμηση λίστας σε φθίνουσα σειρά πλήθους χαρακτήρων
-    
+    sort(v.begin(), v.end(), [](string a, string b) { return a.length() > b.length(); });
     cout << "####################" << endl;
     for (auto x : v)
     {
-        cout << x << " ";
+        cout << x << "(" << x.length() << ") ";
     }
     cout << endl;
 }
