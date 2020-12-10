57a28a4d9c527bd32cf9668e54028f631399cd93
Christos Gogos <chgogos@gmail.com>
2020-12-10 09:17:54 +0200

    lambda

---
+++ lc/live_coding_20201210.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    auto lambda = [](int x) { cout << x * x << endl; };
+    vector<int> v{1, 2, 3, 4, 5};
+    for_each(v.begin(), v.end(), lambda);
+}
