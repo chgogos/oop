5dee5192addfc1245c5ba65a733b33f44d26cd21
Christos Gogos <chgogos@gmail.com>
2020-12-17 10:28:41 +0200

    upd

--- lc/Example1.java
+++ lc/Example1.java
@@ -1,17 +1,21 @@
+class MyClass {
+    public int data;
+
+    public MyClass(int d) {
+        data = d;
+        System.out.println("MyClass object created:" + this);
+    }
+}
+
 public class Example1 {
 
     public static void test() {
-        int a[][] = new int[3][4];
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.printf("%4d", a[i][j]);
-            }
-            System.out.println();
-        }
+        MyClass obj = new MyClass(42);
+        System.out.println(obj.data);
     }
-    
+
     public static void main(String[] args) {
-		test();
-	}
+        test();
+    }
 
 }
