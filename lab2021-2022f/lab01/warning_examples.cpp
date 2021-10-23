60ea6be58d65736b7ce5d383a3b3da6aeaaf00c8
Christos Gogos <chgogos@gmail.com>
2021-10-23 09:51:53 +0300

    example warnings

--- lab2021-2022f/lab01/warning_examples.cpp
+++ lab2021-2022f/lab01/warning_examples.cpp
@@ -7,6 +7,6 @@ int main()
     int a = 0; //  unused variable 'a'
     int sum;
     sum += 10; // 'sum' is used uninitialized in this function
-
+    cout << sum << endl;
     cout << "hello" << endl;
 }
