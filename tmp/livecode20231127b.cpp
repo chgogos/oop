bc6f9b0d8944b41b918dec733460ee9a50619ece
Christos Gogos <cgogos@uoi.gr>
2023-11-27 14:58:22 +0200

    examples

---
+++ tmp/livecode20231127b.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class O>
+void fun(vector<O> v, O x){
+    for(int i=0;i<v.size();i++){
+        if (v[i]==x){
+            cout << "Position " << i << endl;
+            return;
+        }
+    }
+    cout << "Not found" << endl;
+}
+
+class Person{
+    private:
+        string name;
+    public:
+        Person(string n) : name(n) {}
+        string get_name() const{
+            return name;
+        }
+};
+
+bool operator==(const Person &p1, const Person &p2){
+    return p1.get_name() == p2.get_name();
+}
+
+int main(){
+    vector<int> v = {7,3,4,2,1,9};
+    fun(v, 9);
+    
+    vector<string> v2 = {"nikos", "maria"};
+    fun(v2, string("nikos"));
+
+    vector<Person> v3 ={Person("Kostas"), Person("Niki"), Person("Giannis")};
+    fun(v3, Person("Nikiforos"));
+}
