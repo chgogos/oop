c1d847c667c79acefb2c73036e1259c39ee0fce4
Christos Gogos <chgogos@gmail.com>
2025-11-12 14:47:53 +0200

    oop12

--- various/COP3330/lect12/exercise1.cpp
+++ various/COP3330/lect12/exercise1.cpp
@@ -5,34 +5,41 @@ class Distance {
   double meters;
 
  public:
-  Distance() : meters(0) {} // 1
+  Distance() : meters(0) { cout << "Distance() called" << endl; }
 
-  Distance(int m) : meters(m) {} // 2
+  Distance(int m) : meters(m) { cout << "Distance(int) called" << endl; }
 
-  Distance(double km) : meters(km * 1000.0) {} // 3
+  Distance(double km) : meters(km * 1000.0) {
+    cout << "Distance(double) called" << endl;
+  }
 
-  Distance(const Distance& other) : meters(other.meters) {} // 4
+  Distance(const Distance& other) : meters(other.meters) {
+    cout << "Distance(const Distance&) called" << endl;
+  }
 
-  // 5
   Distance& operator=(const Distance& other) {
+    cout << "operator= called" << endl;
     meters = other.meters;
     return *this;
   }
 
-  void print() const { cout << meters << " meters\n"; }
+  friend ostream& operator<<(ostream& os, const Distance& other) {
+    os << "Distance: " << other.meters << "m";
+    return os;
+  }
 };
 
 int main() {
-  Distance d1;          // 1
-  Distance d2 = 300;    // 2 
-  Distance d3(250);     // 2
-  Distance d4 = d2;     // 4
-  Distance d5 = 0.5;    // 3
-  d1 = d3;              // 5
-  d1.print(); // 250 meters
-  d2.print(); // 300 meters
-  d3.print(); // 250 meters
-  d4.print(); // 300 meters
-  d5.print(); // 500 meters
+  Distance d1;        // Distance() called
+  Distance d2(250);   // Distance(int) called
+  Distance d3 = 300;  // Distance(int) called
+  Distance d4(2.5);   // Distance(double) called
+  Distance d5 = 7.8;  // Distance(double) called
+  Distance d6(d1);    // Distance(const Distance&) called
+  Distance d7 = d2;   // Distance(const Distance&) called
+  d1 = d3;            // operator= called
+  for (const Distance *d : {&d1, &d2, &d3, &d4, &d5, &d6, &d7}) {
+    cout << *d << endl;
+  }
   return 0;
 }
